f07f5a20cbfc5eb35691795e98cb33cc5ccfb522
Boris Kirov - INFO <boris@info.nl>
2020-04-18 11:52:42 +0200

    Added svg icon

--- src/components/button.js
+++ src/components/button.js
@@ -3,9 +3,6 @@ import "../styles/button.css"
 import "../styles/styles.css"
 
 const Button = ({ children }) => (
-    // <button>
-    //   {children}
-    // </button>
       <button type="button">
         <a href="https://github.com/sboriskirov/fluiditype" class="header" target="_blank" rel="noopener noreferrer">
           {children}
